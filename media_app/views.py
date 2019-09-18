39bd405e8ed3208e9d51363c303457797452da5d
kazuma-seki <kazuma444756@gmail.com>
2019-09-19 03:12:26 +0900

    no message

--- media_app/views.py
+++ media_app/views.py
@@ -5,22 +5,41 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from accounts.models import CustomUser
-#from accounts.forms import CustomUserCreationForm
-#from accounts.models import CustomUser
-# Create your views here.
+from .models import Friend
+from django.contrib import messages
+from django.db.models import Q
+from django.views import generic
+from django.urls import reverse_lazy
 
-def up_load(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            #form.save(commit=false)
-            form.author = request.user
-            form.save()
-            return redirect('home')
-            #return HttpResponseRedirect('')#レダイレクト先はhome画面へ遷移
-    else:
-        form = DocumentForm()
-    return render(request, 'media_app/up_load.html', {'form': form,'author': request.user})
+class up_load(generic.CreateView):
+    # アップロード
+    def get_form_kwargs(self, *args, **kwargs):
+        form_kwargs = super().get_form_kwargs(*args, **kwargs)
+        form_kwargs['initial'] = {'author': self.request.user}  # フォームに初期値を設定する。
+        return form_kwargs
+
+    model = Document
+    form_class = DocumentForm
+    success_url = reverse_lazy('home')
+    template_name = 'media_app/up_load.html'
+
+# def up_load(request):
+#     #POSTauthorが空の時はできない
+#     #form = DocumentForm(request.POST, request.FILES,{'author': request.user})
+#     if request.method == 'POST':
+#         form = DocumentForm(request.POST, request.FILES,{'author': request.user})
+#         if form.is_valid():
+#             #form.save(commit=false)
+#             print("debug01")
+#             form.author = request.user
+#             print(form.author)
+#             form.save()
+#             return redirect('home')
+#             #return HttpResponseRedirect('')#レダイレクト先はhome画面へ遷移
+#     else:
+#         print("debug02")
+#         form = DocumentForm()
+#     return render(request, 'media_app/up_load.html', {'form': form,'author': request.user})
 
 def view(request):
     obj = Document.objects.all()
@@ -32,3 +51,34 @@ def view(request):
         'objs': objs,
     }
     return render(request,'media_app/view.html',params)
+
+# Friendの追加処理
+def add(request):
+    # 追加するUserを取得
+    add_name = request.GET['name']
+    add_user = CustomUser.objects.filter(username=add_name).first()
+    # Userが本人だった場合の処理
+    print("debug01")
+    if add_user == request.user:
+        messages.info(request, "自分自身をFriendに追加することはできません。")
+        return redirect(to='view')
+    # publicの取得
+    #(public_user, public_group) = get_public()
+    # add_userのFriendの数を調べる
+    print("debug02")
+    frd_num = Friend.objects.filter(owner=request.user).filter(friend=add_user).count()
+    # ゼロより大きければ既に登録済み
+    if frd_num > 0:
+        messages.info(request, add_user.username + ' は既に追加されています。')
+        return redirect(to='view')
+
+    # ここからFriendの登録処理
+    print("debug03")
+    frd = Friend()
+    frd.owner = request.user
+    frd.friend = add_user
+    #frd.group = public_group
+    frd.save()
+    # メッセージを設定
+    messages.success(request, add_user.username + ' を追加しました！ groupページに移動して、追加したFriendをメンバーに設定して下さい。')
+    return redirect(to='view')
