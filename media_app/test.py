0c2e45a295b060677bd344fb0b70e732f126e18b
makao26 <kazuma444756@gmail.com>
2019-10-26 01:19:10 +0900

    テストコード　画像アップロードのcreate 作成

--- media_app/test.py
+++ media_app/test.py
@@ -7,39 +7,17 @@ from django.contrib.auth import get_user_model
 import os
 
 class DocumentAssertion(TestCase):
-    def assertDocmentModel(self, actual_docment, author, title, description, photo, uploaded_at):
-        self.assertEqual(actual_docment.author, author)
-        self.assertEqual(actual_docment.title, title)
-        self.assertEqual(actual_docment.description, description)
-        self.assertEqual(actual_docment.photo, photo)
-        #self.assertEqual(actual_docment.uploaded_at, uploaded_at)
-
-class TestDocumentModel(TestCase):
-    def creating_a_docment_and_saving(self, author, title, description, photo,uploaded_at):
-        document = Document()
-        # if name is not None:
-        #     document.author = author
-        # if title is not None:
-        #     document.title = title
-        # if description is not None:
-        #     document.description = description
-        # if description is not None:
-        #     document.description = description
-        document.save()
-
     def test_is_empty(self):
         saved_docments = Document.objects.all()
         self.assertEqual(saved_docments.count(), 0)
 
-    def test_is_not_empty(self):
-        author = get_user_model().objects.create_user(username='admin', email='kazuma444756@gmail.com', password='kazuma123')
+    def test_is_create_docment(self):
+        author = CustomUser.objects.create(username='test',email='test@gmail.com',password='test0123')
+        print(author)
         title = "test"
-        description = "test model"
+        description = "test create model"
         path = os.getcwd()
-        photo = SimpleUploadedFile(name='test_img.jpg', content=open(path+"/media_test/test_img.jpg", 'rb').read(), content_type='image/jpeg')
-        upload_at = None
-        self.creating_a_docment_and_saving(author,title,description,photo,upload_at)
-        saved_docments = Document.objects.all()
-        self.assertEqual(saved_docments.count(), 1)
-        actual_docment = saved_docments[0]
-        self.assertDocmentModel(actual_docment,author,title,description,photo,upload_at)
+        photo = SimpleUploadedFile(name='test_img.jpg', content=open(os.path.join(path, 'media_test/test_img.jpg'),'rb').read(), content_type='image/jpeg')
+        uploaded_at = None
+        Document.objects.create(author=author,title = title,description = description,photo = photo,uploaded_at = uploaded_at)
+        self.assertEqual(1, len(Document.objects.all()))
